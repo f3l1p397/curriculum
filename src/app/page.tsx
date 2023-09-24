e9162cd955370e75da33da1312aad7ae20eb7bac
f3l1p397 <juanfehidalgo@gmail.com>
2023-09-24 01:39:17 -0500

    lista de habilidades

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 import Inicio from "@/components/inicio";
 import Experiencias from "@/components/experiencias";
+import Habilidades from "@/components/habilidades";
 
 export default function Home() {
   return (
     <>
       <Inicio />
-      <Experiencias />
+      <div className="tile is-ancestor px-6">
+        <div className="tile is-child is-9 ">
+          <Experiencias />
+        </div>
+        <div className="tile is-child">
+          <Habilidades />
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/lib/habilidades.tsx
@@ -0,0 +1,37 @@
+export const habilidades = [
+  {
+    id: 1,
+    habilidad: "Metodologías Ágiles",
+    list: "Scrum y Trello.",
+  },
+  {
+    id: 2,
+    habilidad: "Frontend",
+    list: "Vue.js, JavaScript, TypeScript, HTML, CSS, Sass, BootStrap y Bulma.",
+  },
+  {
+    id: 3,
+    habilidad: "Backend",
+    list: "Python, Node.js, Express.js y Laravel.",
+  },
+  {
+    id: 4,
+    habilidad: "Base de datos",
+    list: "PostgreSQL, MySQL y Oracle y Firebase",
+  },
+  {
+    id: 5,
+    habilidad: "Control de versiones",
+    list: "Git y GitHub.",
+  },
+  {
+    id: 6,
+    habilidad: "Herramientas",
+    list: "Insomnia, y Postman.",
+  },
+  {
+    id: 7,
+    habilidad: "Sistemas Operativos",
+    list: "Windows y Linux.",
+  },
+];
