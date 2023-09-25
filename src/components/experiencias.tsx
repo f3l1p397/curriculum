c22b113f669d9bf7a23838b02971d1ee62b4659d
f3l1p397 <juanfehidalgo@gmail.com>
2023-09-24 23:06:50 -0500

    primera animacion

--- src/components/experiencias.tsx
+++ src/components/experiencias.tsx
@@ -1,12 +1,27 @@
+"use client";
 import { experiencias } from "@/lib/experiencias";
 import Experiencia from "./experiencia";
 import styles from "@/styles/fieldsets.module.css";
+import anime from "animejs/lib/anime.es.js";
+import { useRef, useEffect } from "react";
 export default function Experiencias() {
+  // const animation = useRef(null);
+  useEffect(() => {
+    anime({
+      targets: ".mover",
+      // loop: true,
+      // translateX: 250,
+      rotate: "1turn",
+      // backgroundColor: "#FFF",
+      duration: 800,
+    });
+  });
+
   return (
     <>
       <fieldset className={styles.field_habilidades}>
         <legend className={styles.legend_habilidades}> Experiencia</legend>
-        <div className="columns is-mobile is-desktop is-multiline px-1">
+        <div className="mover columns is-mobile is-desktop is-multiline px-1">
           {experiencias.map(
             ({ id, empresa, cargo, inicio, fin, actividades }) => {
               return (
